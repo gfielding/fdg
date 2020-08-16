278ff372d14b7752adf71168c23f089875323038
Greg Fielding <greg@fieldingdesigngroup.com>
2020-08-16 14:20:26 -0700

    first build

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,3 +16,25 @@ export default {
   }
 }
 </script>
+
+<style>
+.home	{
+	position: relative;
+}
+img {
+	position: absolute;
+	top: 10vh;
+	left: 0;
+	animation: move 3s 1s both ease-in-out 2;
+}
+
+@keyframes move {
+	0% {
+		transform: translateX(-20vw);
+	}
+
+	100% {
+		transform: translateX(120vw);
+	}
+}
+</style>
